9789544356f760b60a8f5bed4da4701babdb833d
Andreas Glatz <glatzan@gmail.com>
2020-03-15 21:01:45 +0100

    Histogram fix

--- src/app/components/workView/histo-view/histo-view.component.ts
+++ src/app/components/workView/histo-view/histo-view.component.ts
@@ -6,6 +6,8 @@ import {ImageService} from "../../../service/image.service";
 import {FilterHelper} from "../../../worker/filter/filter-helper";
 import DrawUtil from "../../../utils/draw-util";
 import {InitializeFilter} from "../../../worker/filter/initialize-filter";
+import {iif, of} from "rxjs";
+import {flatMap} from "rxjs/operators";
 
 @Component({
   selector: 'app-histo-view',
@@ -34,29 +36,36 @@ export class HistoViewComponent implements OnInit {
     this.workViewService.onRenderImageTools.subscribe(renderHistogram => {
       this.renderHistogram = renderHistogram;
       if (renderHistogram && this.displayImage) {
-        this.showHistogram(this.displayImage.id);
+        this.showHistogram(this.displayImage);
       }
     });
 
     this.workViewService.onChangeDisplayImage.subscribe(image => {
       this.displayImage = image;
 
-        if (this.renderHistogram) {
-          this.showHistogram(this.displayImage.id);
-        }
-      });
+      if (this.renderHistogram) {
+        this.showHistogram(this.displayImage);
+      }
+    });
   }
 
 
-  showHistogram(imageID: string) {
-    this.workViewService.getPNGFromBuffer(imageID).pipe(
+  showHistogram(image: CImage) {
+    of(image).pipe(
+      flatMap((image: CImage) =>
+        iif(() => image.id === "tmp",
+          of(image),
+          this.workViewService.getPNGFromBuffer(image.id)
+        )
+      ),
       (new InitializeFilter(null).doFilter()),
-      (new HistogramFilter(null).doFilter(0, 0, {targetData: "histogram"})),
+      (new HistogramFilter(null).doFilter(0, 0, {targetData: "histogram"}))
     ).subscribe(x => {
       const cx = FilterHelper.get2DContext(this.canvas.nativeElement);
       DrawUtil.drawRect(cx, 0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height, "#fff");
       HistogramFilter.drawHistogram(this.canvas.nativeElement, x.getData("histogram").data);
       this.histogramData = x.getData("histogram");
+      console.log("Histogram")
       console.log(x)
     })
   }
